89ed7456213539221bc41baaf2c178f1d6ce54bb
FJBueno94 <fernando_jordi@hotmail.com>
2022-10-17 19:26:26 -0300

    adiciona o requisito 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,12 @@
+db.getCollection("produtos").updateMany(
+  {
+      nome: { $in: ["Big Mac", "Quarteirão com Queijo"] },
+  },
+  {
+      $push: 
+          {
+             ingredientes: "bacon",
+          },
+  },
+);
+db.getCollection("produtos").find({}, { nome: 1, ingredientes: 1, _id: 0 });
